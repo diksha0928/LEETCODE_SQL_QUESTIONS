e6642f40dc8627ddb9748713b4dcdbdd1f719d8d
Diksha Pandey <124272668+diksha0928@users.noreply.github.com>
2026-02-02 22:20:47 +0530

    Time: 555 ms (43.84%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+select u.user_id as buyer_id,
+       u.join_date,
+       count(o.order_date) as orders_in_2019
+from Users u
+left join Orders o
+on u.user_id = o.buyer_id
+and extract(year from o.order_date) = 2019
+group by u.user_id, 
+         u.join_date;
