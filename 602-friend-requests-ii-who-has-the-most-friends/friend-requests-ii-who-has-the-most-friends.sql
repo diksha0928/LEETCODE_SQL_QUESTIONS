1a688635ea239cf0c46b593cba1536cb18624228
Diksha Pandey <124272668+diksha0928@users.noreply.github.com>
2026-02-04 19:17:32 +0530

    Time: 179 ms (50.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+select id, count(*) as num
+from (
+    select requester_id as id from RequestAccepted
+    union all
+    select accepter_id as id from RequestAccepted
+)
+group by id
+order by num desc
+limit 1;
