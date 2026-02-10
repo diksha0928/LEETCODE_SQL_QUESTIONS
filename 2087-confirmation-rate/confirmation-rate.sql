8b9c1e6cece15ebb88d376f094ab0faf9e8c54c0
Diksha Pandey <124272668+diksha0928@users.noreply.github.com>
2026-02-10 11:02:24 +0530

    Time: 308 ms (11.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,17 @@
+-- Write your PostgreSQL query statement below
+select s.user_id,
+       round(
+            coalesce(
+                avg(
+                    case
+                        when c.action = 'confirmed' then 1.00
+                        else 0
+                        end
+                ),0
+            ),2   
+             )
+       as confirmation_rate
+from Signups s
+left join Confirmations c
+on s.user_id = c.user_id
+group by s.user_id;
