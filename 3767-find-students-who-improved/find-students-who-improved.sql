57807ff2ce7e296b007f13d94823173a86b46b36
Diksha Pandey <124272668+diksha0928@users.noreply.github.com>
2026-02-10 11:41:50 +0530

    Time: 217 ms (18.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -0,0 +1,30 @@
+-- Write your PostgreSQL query statement below
+with highest_scores as(
+    select student_id,
+    subject,
+    score,
+    exam_date,
+    first_value(score) over (
+        partition by student_id, subject
+        order by exam_date
+    ) as first_score,
+    last_value(score) over (
+        partition by student_id, subject
+        rows between unbounded preceding and unbounded following
+        -- order by exam_date
+    ) as latest_score,
+    count(*) over (
+        partition by student_id, subject
+    ) as exam_count
+from Scores
+)
+
+select student_id,
+       subject,
+       first_score,
+       latest_score
+from highest_scores
+where exam_count >= 2
+and latest_score > first_score
+group by student_id, subject,first_score, latest_score
+order by student_id, subject asc;
