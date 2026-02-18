1cbcd89fcbbc7e532e1ed2525bece4ac2315712d
Diksha Pandey <124272668+diksha0928@users.noreply.github.com>
2026-02-18 17:44:04 +0530

    Time: 207 ms (84.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT)
+RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  IF N <= 0 THEN
+    RETURN QUERY SELECT NULL::INT;
+  ELSE
+    RETURN QUERY
+    SELECT DISTINCT e.salary
+    FROM Employee e
+    ORDER BY e.salary DESC
+    LIMIT 1 OFFSET N - 1;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
