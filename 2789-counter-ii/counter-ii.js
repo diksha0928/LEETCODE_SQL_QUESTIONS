c8a70c7dd9b61c35a1d59f7f9a55ea940138faa6
Diksha Pandey <124272668+diksha0928@users.noreply.github.com>
2026-03-20 21:44:32 +0530

    Time: 50 ms (57.26%) | Memory: 56.8 MB (39.57%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -18,10 +18,3 @@ var createCounter = function(init) {
         }
     };
 };
-
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
