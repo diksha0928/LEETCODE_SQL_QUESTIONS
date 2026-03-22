081080640dfe645c3c932953d3a9ebcf37114f73
Diksha Pandey <124272668+diksha0928@users.noreply.github.com>
2026-03-22 19:35:08 +0530

    Time: 39 ms (82.14%) | Memory: 53.2 MB (82.01%) - LeetSync

--- 2796-allow-one-function-call/allow-one-function-call.js
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -13,10 +13,4 @@ var once = function(fn) {
     }
 };
 
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+
